fcaaa1e5668b24f7ff0b5ec0e7528668861fa23b
darinbarnes <barnes.darin@gmail.com>
2017-12-04 19:45:06 -0500

    Homepage Checkpoint 171204

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,13 @@
-// This is where it all goes :)
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+$(window).load(function() {
+   $('div').each(function(i) {
+      $(this).delay((i + 1) * 250).fadeIn(2000);
+   });
+});
