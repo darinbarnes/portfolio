6cb2f20363829931daec8416e31296cbd9a5262c
darinbarnes <barnes.darin@gmail.com>
2017-12-06 23:57:11 -0500

    Homepage Edits

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -5,9 +5,3 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
-
-$(window).load(function() {
-   $('div').each(function(i) {
-      $(this).delay((i + 1) * 250).fadeIn(2000);
-   });
-});
